705aebcc19fe38b3bddf93cdaf9af927cdbdb265
nguyenvando30 <do30032004@gmail.com>
2025-08-27 08:25:00 +0700

    feat: add config

--- main/example_rgb_avoid_tearing.c
+++ main/example_rgb_avoid_tearing.c
@@ -22,52 +22,52 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#define EXAMPLE_LCD_H_RES               (480)
-#define EXAMPLE_LCD_V_RES               (480)
-#define EXAMPLE_LCD_BIT_PER_PIXEL       (18)
-#define EXAMPLE_RGB_BIT_PER_PIXEL       (16)
-#define EXAMPLE_RGB_DATA_WIDTH          (16)
-#define EXAMPLE_RGB_BOUNCE_BUFFER_SIZE  (EXAMPLE_LCD_H_RES * CONFIG_EXAMPLE_LCD_RGB_BOUNCE_BUFFER_HEIGHT)
-#define EXAMPLE_LCD_IO_RGB_DISP         (-1)             // -1 if not used
-#define EXAMPLE_LCD_IO_RGB_VSYNC        (GPIO_NUM_3)
-#define EXAMPLE_LCD_IO_RGB_HSYNC        (GPIO_NUM_46)
-#define EXAMPLE_LCD_IO_RGB_DE           (GPIO_NUM_0)
-#define EXAMPLE_LCD_IO_RGB_PCLK         (GPIO_NUM_9)
-#define EXAMPLE_LCD_IO_RGB_DATA0        (GPIO_NUM_14)
-#define EXAMPLE_LCD_IO_RGB_DATA1        (GPIO_NUM_13)
-#define EXAMPLE_LCD_IO_RGB_DATA2        (GPIO_NUM_12)
-#define EXAMPLE_LCD_IO_RGB_DATA3        (GPIO_NUM_11)
-#define EXAMPLE_LCD_IO_RGB_DATA4        (GPIO_NUM_10)
-#define EXAMPLE_LCD_IO_RGB_DATA5        (GPIO_NUM_39)
-#define EXAMPLE_LCD_IO_RGB_DATA6        (GPIO_NUM_38)
-#define EXAMPLE_LCD_IO_RGB_DATA7        (GPIO_NUM_45)
-#define EXAMPLE_LCD_IO_RGB_DATA8        (GPIO_NUM_48)
-#define EXAMPLE_LCD_IO_RGB_DATA9        (GPIO_NUM_47)
-#define EXAMPLE_LCD_IO_RGB_DATA10       (GPIO_NUM_21)
-#define EXAMPLE_LCD_IO_RGB_DATA11       (GPIO_NUM_1)
-#define EXAMPLE_LCD_IO_RGB_DATA12       (GPIO_NUM_2)
-#define EXAMPLE_LCD_IO_RGB_DATA13       (GPIO_NUM_42)
-#define EXAMPLE_LCD_IO_RGB_DATA14       (GPIO_NUM_41)
-#define EXAMPLE_LCD_IO_RGB_DATA15       (GPIO_NUM_40)
-#define EXAMPLE_LCD_IO_RGB_DATA16      (GPIO_NUM_5)
-#define EXAMPLE_LCD_IO_RGB_DATA17      (GPIO_NUM_6)
-#define EXAMPLE_LCD_IO_SPI_CS           (GPIO_NUM_4)
-#define EXAMPLE_LCD_IO_SPI_SCL          (GPIO_NUM_7)
-#define EXAMPLE_LCD_IO_SPI_SDA          (GPIO_NUM_15)
-#define EXAMPLE_LCD_IO_RST              (GPIO_NUM_16)             // -1 if not used
-#define EXAMPLE_PIN_NUM_BK_LIGHT        (-1)    // -1 if not used
-#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL   (1)
-#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL  !EXAMPLE_LCD_BK_LIGHT_ON_LEVEL
+#define EXAMPLE_LCD_H_RES (480)
+#define EXAMPLE_LCD_V_RES (480)
+#define EXAMPLE_LCD_BIT_PER_PIXEL (18)
+#define EXAMPLE_RGB_BIT_PER_PIXEL (16)
+#define EXAMPLE_RGB_DATA_WIDTH (16)
+#define EXAMPLE_RGB_BOUNCE_BUFFER_SIZE (EXAMPLE_LCD_H_RES * CONFIG_EXAMPLE_LCD_RGB_BOUNCE_BUFFER_HEIGHT)
+#define EXAMPLE_LCD_IO_RGB_DISP (-1) // -1 if not used
+#define EXAMPLE_LCD_IO_RGB_VSYNC (GPIO_NUM_3)
+#define EXAMPLE_LCD_IO_RGB_HSYNC (GPIO_NUM_46)
+#define EXAMPLE_LCD_IO_RGB_DE (GPIO_NUM_0)
+#define EXAMPLE_LCD_IO_RGB_PCLK (GPIO_NUM_9)
+#define EXAMPLE_LCD_IO_RGB_DATA0 (GPIO_NUM_14)
+#define EXAMPLE_LCD_IO_RGB_DATA1 (GPIO_NUM_13)
+#define EXAMPLE_LCD_IO_RGB_DATA2 (GPIO_NUM_12)
+#define EXAMPLE_LCD_IO_RGB_DATA3 (GPIO_NUM_11)
+#define EXAMPLE_LCD_IO_RGB_DATA4 (GPIO_NUM_10)
+#define EXAMPLE_LCD_IO_RGB_DATA5 (GPIO_NUM_39)
+#define EXAMPLE_LCD_IO_RGB_DATA6 (GPIO_NUM_38)
+#define EXAMPLE_LCD_IO_RGB_DATA7 (GPIO_NUM_45)
+#define EXAMPLE_LCD_IO_RGB_DATA8 (GPIO_NUM_48)
+#define EXAMPLE_LCD_IO_RGB_DATA9 (GPIO_NUM_47)
+#define EXAMPLE_LCD_IO_RGB_DATA10 (GPIO_NUM_21)
+#define EXAMPLE_LCD_IO_RGB_DATA11 (GPIO_NUM_1)
+#define EXAMPLE_LCD_IO_RGB_DATA12 (GPIO_NUM_2)
+#define EXAMPLE_LCD_IO_RGB_DATA13 (GPIO_NUM_42)
+#define EXAMPLE_LCD_IO_RGB_DATA14 (GPIO_NUM_41)
+#define EXAMPLE_LCD_IO_RGB_DATA15 (GPIO_NUM_40)
+#define EXAMPLE_LCD_IO_RGB_DATA16 (GPIO_NUM_5)
+#define EXAMPLE_LCD_IO_RGB_DATA17 (GPIO_NUM_6)
+#define EXAMPLE_LCD_IO_SPI_CS (GPIO_NUM_4)
+#define EXAMPLE_LCD_IO_SPI_SCL (GPIO_NUM_7)
+#define EXAMPLE_LCD_IO_SPI_SDA (GPIO_NUM_15)
+#define EXAMPLE_LCD_IO_RST (GPIO_NUM_16) // -1 if not used
+#define EXAMPLE_PIN_NUM_BK_LIGHT (-1)    // -1 if not used
+#define EXAMPLE_LCD_BK_LIGHT_ON_LEVEL (1)
+#define EXAMPLE_LCD_BK_LIGHT_OFF_LEVEL !EXAMPLE_LCD_BK_LIGHT_ON_LEVEL
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////// Please update the following configuration according to your touch spec ////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #if CONFIG_EXAMPLE_LCD_TOUCH_CONTROLLER_GT911
-#define TOUCH_HOST                      (I2C_NUM_0)
-#define EXAMPLE_PIN_NUM_TOUCH_SCL       (GPIO_NUM_45)
-#define EXAMPLE_PIN_NUM_TOUCH_SDA       (GPIO_NUM_19)
-#define EXAMPLE_PIN_NUM_TOUCH_RST       (-1)            // -1 if not used
-#define EXAMPLE_PIN_NUM_TOUCH_INT       (-1)            // -1 if not used
+#define TOUCH_HOST (I2C_NUM_0)
+#define EXAMPLE_PIN_NUM_TOUCH_SCL (GPIO_NUM_45)
+#define EXAMPLE_PIN_NUM_TOUCH_SDA (GPIO_NUM_19)
+#define EXAMPLE_PIN_NUM_TOUCH_RST (-1) // -1 if not used
+#define EXAMPLE_PIN_NUM_TOUCH_INT (-1) // -1 if not used
 #endif
 
 static const char *TAG = "example";
@@ -78,42 +78,63 @@ IRAM_ATTR static bool rgb_lcd_on_vsync_event(esp_lcd_panel_handle_t panel, const
 }
 
 static const st7701_lcd_init_cmd_t lcd_init_cmds[] = {
-//  {cmd, { data }, data_size, delay_ms}
-    {0xFF, (uint8_t []){0x77, 0x01, 0x00, 0x00, 0x10}, 5, 0},
-    {0xC0, (uint8_t []){0x3B, 0x00}, 2, 0},
-    {0xC1, (uint8_t []){0x0D, 0x02}, 2, 0},
-    {0xC2, (uint8_t []){0x31, 0x05}, 2, 0},
-    {0xCD, (uint8_t []){0x00}, 1, 0},
-    {0xB0, (uint8_t []){0x00, 0x11, 0x18, 0x0E, 0x11, 0x06, 0x07, 0x08, 0x07, 0x22, 0x04, 0x12, 0x0F, 0xAA, 0x31, 0x18}, 16, 0},
-    {0xB1, (uint8_t []){0x00, 0x11, 0x19, 0x0E, 0x12, 0x07, 0x08, 0x08, 0x08, 0x22, 0x04, 0x11, 0x11, 0xA9, 0x32, 0x18}, 16, 0},
-    {0xFF, (uint8_t []){0x77, 0x01, 0x00, 0x00, 0x11}, 5, 0},
-    {0xB0, (uint8_t []){0x60}, 1, 0},
-    {0xB1, (uint8_t []){0x32}, 1, 0},
-    {0xB2, (uint8_t []){0x07}, 1, 0},
-    {0xB3, (uint8_t []){0x80}, 1, 0},
-    {0xB5, (uint8_t []){0x49}, 1, 0},
-    {0xB7, (uint8_t []){0x85}, 1, 0},
-    {0xB8, (uint8_t []){0x21}, 1, 0},
-    {0xC1, (uint8_t []){0x78}, 1, 0},
-    {0xC2, (uint8_t []){0x78}, 1, 0},
-    {0xE0, (uint8_t []){0x00, 0x1B, 0x02}, 3, 0},
-    {0xE1, (uint8_t []){0x08, 0xA0, 0x00, 0x00, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x44, 0x44}, 11, 0},
-    {0xE2, (uint8_t []){0x11, 0x11, 0x44, 0x44, 0xED, 0xA0, 0x00, 0x00, 0xEC, 0xA0, 0x00, 0x00}, 12, 0},
-    {0xE3, (uint8_t []){0x00, 0x00, 0x11, 0x11}, 4, 0},
-    {0xE4, (uint8_t []){0x44, 0x44}, 2, 0},
-    {0xE5, (uint8_t []){0x0A, 0xE9, 0xD8, 0xA0, 0x0C, 0xEB, 0xD8, 0xA0, 0x0E, 0xED, 0xD8, 0xA0, 0x10, 0xEF, 0xD8, 0xA0}, 16, 0},
-    {0xE6, (uint8_t []){0x00, 0x00, 0x11, 0x11}, 4, 0},
-    {0xE7, (uint8_t []){0x44, 0x44}, 2, 0},
-    {0xE8, (uint8_t []){0x09, 0xE8, 0xD8, 0xA0, 0x0B, 0xEA, 0xD8, 0xA0, 0x0D, 0xEC, 0xD8, 0xA0, 0x0F, 0xEE, 0xD8, 0xA0}, 16, 0},
-    {0xEB, (uint8_t []){0x02, 0x00, 0xE4, 0xE4, 0x88, 0x00, 0x40}, 7, 0},
-    {0xEC, (uint8_t []){0x3C, 0x00}, 2, 0},
-    {0xED, (uint8_t []){0xAB, 0x89, 0x76, 0x54, 0x02, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x20, 0x45, 0x67, 0x98, 0xBA}, 16, 0},
-    {0xFF, (uint8_t []){0x77, 0x01, 0x00, 0x00, 0x13}, 5, 0},
-    {0xE5, (uint8_t []){0xE4}, 1, 0},
-    {0xFF, (uint8_t []){0x77, 0x01, 0x00, 0x00, 0x00}, 5, 0},
-    {0x11, (uint8_t []){0x00}, 0, 120},
-    {0x29, (uint8_t []){0x00}, 0, 0},
+    //  {cmd, { data }, data_size, delay_ms}
+    {0xFF, (uint8_t[]){0x77, 0x01, 0x00, 0x00, 0x10}, 5, 0},
+    {0xC0, (uint8_t[]){0x3B, 0x00}, 2, 0},
+    {0xC1, (uint8_t[]){0x0D, 0x02}, 2, 0},
+    {0xC2, (uint8_t[]){0x31, 0x05}, 2, 0},
+    {0xCD, (uint8_t[]){0x08}, 1, 0},
+    {0x3A, (uint8_t[]){0x60}, 1, 0},
+    {0xB0, (uint8_t[]){0x00, 0x11, 0x18, 0x0E, 0x11, 0x06, 0x07, 0x08, 0x07, 0x22, 0x04, 0x12, 0x0F, 0xAA, 0x31, 0x18}, 16, 0},
+    {0xB1, (uint8_t[]){0x00, 0x11, 0x19, 0x0E, 0x12, 0x07, 0x08, 0x08, 0x08, 0x22, 0x04, 0x11, 0x11, 0xA9, 0x32, 0x18}, 16, 0},
+    {0xFF, (uint8_t[]){0x77, 0x01, 0x00, 0x00, 0x11}, 5, 0},
+    {0xB0, (uint8_t[]){0x60}, 1, 0},
+    {0xB1, (uint8_t[]){0x32}, 1, 0},
+    {0xB2, (uint8_t[]){0x07}, 1, 0},
+    {0xB3, (uint8_t[]){0x80}, 1, 0},
+    {0xB5, (uint8_t[]){0x49}, 1, 0},
+    {0xB7, (uint8_t[]){0x85}, 1, 0},
+    {0xB8, (uint8_t[]){0x21}, 1, 0},
+    {0xC1, (uint8_t[]){0x78}, 1, 0},
+    {0xC2, (uint8_t[]){0x78}, 1, 0},
+    {0xE0, (uint8_t[]){0x00, 0x1B, 0x02}, 3, 0},
+    {0xE1, (uint8_t[]){0x08, 0xA0, 0x00, 0x00, 0x07, 0xA0, 0x00, 0x00, 0x00, 0x44, 0x44}, 11, 0},
+    {0xE2, (uint8_t[]){0x11, 0x11, 0x44, 0x44, 0xED, 0xA0, 0x00, 0x00, 0xEC, 0xA0, 0x00, 0x00}, 12, 0},
+    {0xE3, (uint8_t[]){0x00, 0x00, 0x11, 0x11}, 4, 0},
+    {0xE4, (uint8_t[]){0x44, 0x44}, 2, 0},
+    {0xE5, (uint8_t[]){0x0A, 0xE9, 0xD8, 0xA0, 0x0C, 0xEB, 0xD8, 0xA0, 0x0E, 0xED, 0xD8, 0xA0, 0x10, 0xEF, 0xD8, 0xA0}, 16, 0},
+    {0xE6, (uint8_t[]){0x00, 0x00, 0x11, 0x11}, 4, 0},
+    {0xE7, (uint8_t[]){0x44, 0x44}, 2, 0},
+    {0xE8, (uint8_t[]){0x09, 0xE8, 0xD8, 0xA0, 0x0B, 0xEA, 0xD8, 0xA0, 0x0D, 0xEC, 0xD8, 0xA0, 0x0F, 0xEE, 0xD8, 0xA0}, 16, 0},
+    {0xEB, (uint8_t[]){0x02, 0x00, 0xE4, 0xE4, 0x88, 0x00, 0x40}, 7, 0},
+    {0xEC, (uint8_t[]){0x3C, 0x00}, 2, 0},
+    {0xED, (uint8_t[]){0xAB, 0x89, 0x76, 0x54, 0x02, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x20, 0x45, 0x67, 0x98, 0xBA}, 16, 0},
+    {0xFF, (uint8_t[]){0x77, 0x01, 0x00, 0x00, 0x13}, 5, 0},
+    {0xE5, (uint8_t[]){0xE4}, 1, 0},
+    {0xFF, (uint8_t[]){0x77, 0x01, 0x00, 0x00, 0x00}, 5, 0},
+    {0x11, (uint8_t[]){0x00}, 0, 120},
+    {0x29, (uint8_t[]){0x00}, 0, 0},
 };
+
+
+
+void draw_color_example(void)
+{
+    // Tạo một object kiểu box
+    lv_obj_t *color_box = lv_obj_create(lv_scr_act());
+
+    // Đặt kích thước
+    lv_obj_set_size(color_box, 100, 100);
+
+    // Đặt vị trí (center màn hình)
+    lv_obj_center(color_box);
+
+    // Set màu nền (ví dụ: màu đỏ)
+    lv_obj_set_style_bg_color(color_box, lv_color_hex(0x800080), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(color_box, LV_OPA_COVER, LV_PART_MAIN);
+}
+
+
 
 void app_main()
 {
@@ -121,8 +142,7 @@ void app_main()
     ESP_LOGI(TAG, "Turn off LCD backlight");
     gpio_config_t bk_gpio_config = {
         .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = 1ULL << EXAMPLE_PIN_NUM_BK_LIGHT
-    };
+        .pin_bit_mask = 1ULL << EXAMPLE_PIN_NUM_BK_LIGHT};
     ESP_ERROR_CHECK(gpio_config(&bk_gpio_config));
 #endif
 
@@ -179,15 +199,15 @@ void app_main()
     rgb_config.timings.v_res = EXAMPLE_LCD_V_RES;
     st7701_vendor_config_t vendor_config = {
         .rgb_config = &rgb_config,
-        .init_cmds = lcd_init_cmds,      // Uncomment these line if use custom initialization commands
+        .init_cmds = lcd_init_cmds, // Uncomment these line if use custom initialization commands
         .init_cmds_size = sizeof(lcd_init_cmds) / sizeof(lcd_init_cmds[0]),
         .flags = {
-            .auto_del_panel_io = 0,         /**
-                                             * Set to 1 if panel IO is no longer needed after LCD initialization.
-                                             * If the panel IO pins are sharing other pins of the RGB interface to save GPIOs,
-                                             * Please set it to 1 to release the pins.
-                                             */
-            .mirror_by_cmd = 1,             // Set to 0 if `auto_del_panel_io` is enabled
+            .auto_del_panel_io = 0, /**
+                                     * Set to 1 if panel IO is no longer needed after LCD initialization.
+                                     * If the panel IO pins are sharing other pins of the RGB interface to save GPIOs,
+                                     * Please set it to 1 to release the pins.
+                                     */
+            .mirror_by_cmd = 1,     // Set to 0 if `auto_del_panel_io` is enabled
         },
     };
     const esp_lcd_panel_dev_config_t panel_config = {
@@ -258,13 +278,28 @@ void app_main()
 
     ESP_LOGI(TAG, "Display LVGL demos");
     // Lock the mutex due to the LVGL APIs are not thread-safe
-    if (lvgl_port_lock(-1)) {
-        // lv_demo_stress();
-         lv_demo_benchmark();
-        //lv_demo_music();
-        // lv_demo_widgets();
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_DISABLE, // không dùng interrupt
+        .mode = GPIO_MODE_OUTPUT,       // cấu hình output
+        .pin_bit_mask = (1ULL << EXAMPLE_LCD_IO_RGB_DATA16) | (1ULL << EXAMPLE_LCD_IO_RGB_DATA17),
+        .pull_down_en = 0, // không kéo xuống
+        .pull_up_en = 0    // không kéo lên
+    };
+    gpio_config(&io_conf);
+    gpio_set_level(EXAMPLE_LCD_IO_RGB_DATA16, 1);
+    gpio_set_level(EXAMPLE_LCD_IO_RGB_DATA17, 0);
 
-        // Release the mutex
-        lvgl_port_unlock();
-    }
+    // if (lvgl_port_lock(-1))
+    // {
+    //     //  lv_demo_stress();
+    //     // lv_demo_benchmark();
+    //     lv_demo_music();
+    //     // lv_demo_widgets();
+
+    //     // Release the mutex
+    //     lvgl_port_unlock();
+    // }
+
+    draw_color_example();
+
 }
